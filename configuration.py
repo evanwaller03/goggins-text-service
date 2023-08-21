73a1f5a1fb5e2c3587d0fdef6033e3498df1e71d
Evan Waller <92752585+evanwaller03@users.noreply.github.com>
2023-08-21 14:13:47 -0500

    adding the config.py file

---
+++ configuration.py
@@ -0,0 +1,46 @@
+
+
+SECRET_KEY = "YOUR PASSWORD TO YOUR EMAIL"
+spreadsheetId = "YOUR SPREADSHEET ID"
+userEmail = "YOUR EMAIL"
+
+quotes = [
+	"Stay Hard.", 
+	"Don't stop when you're tired. Stop when you're done.", 
+	"You are in danger of living a life so comfortable and soft that you will die without ever realizing your true potential.",
+	"Denial is the ultimate comfort zone.",
+	"Most wars are won or lost in our own heads.",
+	"We’re either getting better or we’re getting worse.",
+	"The hero is you.",
+	"The path to success will leave you callused, bruised, and very tired. It will also leave you empowered.",
+	"The pain that you are willing to endure is measured by how bad you want it.",
+	"It’s time to go to war with yourself.",
+	"We don’t rise to the level of our expectations, we fall to the level of our training.",
+	"There is no better way to grow as a person than to do everyday something you hate.",
+	"Analyze your schedule, kill your empty habits, burn out the bullshit, and see what’s left.",
+	"Never let people who choose the path of least resistance steer you away from your chosen path of most resistance.",
+	"No one is going to come help you. No one’s coming to save you. Be your own hero.",
+	"It’s so easy to be great these days, because everyone else is so weak.",
+	"Nobody cares what you did yesterday. What have you done today to better yourself?",
+	"If you can get through doing things that you hate to do, on the other side is greatness.",
+	"Everybody wants a quick fix. You may get results, but they won’t be permanent. The permanent results come from you having to suffer.",
+	"You have to build calluses on your brain just like how you build calluses on your hands. Callus your mind through pain and suffering.",
+	"The only thing more contagious than a good attitude is a bad one.",
+	"If you want to get better, do the things that no-one else wants to do. Do the things that no-one is even thinking about doing.",
+	"The human body can withstand and accomplish way more than most of us think possible, and that it all begins and ends in the mind.",
+	"In every failure a lot of good things will have happened, and we must acknowledge them.",
+	"You can tolerate doubt as a backseat driver, but if you put doubt in the pilot’s seat, defeat is guaranteed."
+]
+
+carriers = {
+	"Verizon" : "vtext.com",
+	"AT&T" : "txt.att.net",
+	"T-Mobile" : "tmomail.net",
+	"Sprint" : "messaging.sprintpcs.com",
+	"XFinity Mobile" : "vtext.com",
+	"U.S. Cellular" : "email.uscc.net",
+	"Metro PCS" : "mymetropcs.com",
+	"Boost Mobile" : "sms.myboostmobile.com",
+	"Google Fi" : "msg.fi.google.com"
+}
+
